4a40b83ad523011065d73c2928c734a57a0452d4
CORR1T <corsin@ragett.li>
2021-05-27 12:07:58 +0200

    Test commit

--- src/App.tsx
+++ src/App.tsx
@@ -6,15 +6,15 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="logo"/>
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          I wont render
         </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
         >
           Learn React
         </a>

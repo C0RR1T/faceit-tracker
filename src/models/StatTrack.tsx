595f6ca29853ce0a1695132327547403471af715
CORR1T <corsin@ragett.li>
2021-05-28 10:04:04 +0200

    StatTrack.tsx

--- src/models/StatTrack.tsx
+++ src/models/StatTrack.tsx
@@ -1,20 +1,23 @@
 import UserService from "../api/UserService";
 import "../style/Tracker.scss";
-import {URLSearchParams} from "url";
 import React, {useState} from "react";
+import {useLocation} from "react-router";
 
 export default () => {
     let componentState = useState();
-    const params = new URLSearchParams(window.location.search);
+    const params = new URLSearchParams(useLocation().search);
     let [setState, state] = componentState;
     new UserService("", componentState);
-    const background = params.get('background') || 'https://upload.wikimedia.org/wikipedia/commons/9/95/Black_Colour.svg'
+    const background = params.get('background') || '\"https://upload.wikimedia.org/wikipedia/commons/9/95/Black_Colour.svg\"'
     return (
         <>
             <div className="tracker">
-                <h1>Hallo</h1>
+                <h1>Name: C0RR1T</h1>
+                <p>Faceit rank: 10</p>
+                <p>Elo: 1000</p>
+                <p>Elo gained today: +32</p>
             </div>
-            <img src={background} className="background-image" alt="Background image"/>
+            <img src={background.replaceAll('\"', "")} className="background-image" alt="Background image"/>
         </>
     )
 }
